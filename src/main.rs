c74f1139f3d1fe4b7a624fb2fd511c56cce629da
Veritius <veritiusgaming@gmail.com>
2022-09-21 14:52:59 +1000

    First discord stuff

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use poise::serenity_prelude as serenity;
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+pub struct Data {}
+
+pub mod commands;
+use commands::*;
+
+#[tokio::main]
+async fn main() {
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![debug::ping()],
+            ..Default::default()
+        })
+        .token(std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN"))
+        .intents(serenity::GatewayIntents::non_privileged())
+        .user_data_setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data {}) }));
+
+    framework.run().await.unwrap();
+}
